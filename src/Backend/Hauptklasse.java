c783394618ab53f5cb7fb23daee132af2ede85a3
TimJ0212 <tim.jungk@gmx.de>
2021-03-03 22:05:16 +0100

    Struktur hinzugefügt

---
+++ src/Backend/Hauptklasse.java
@@ -0,0 +1,11 @@
+package Backend;
+
+public class Hauptklasse {
+	public static void main(String[] args) {
+	
+	}
+	public static void printeln (String email, String passwort) {
+		System.out.println(email + passwort);
+	}
+	
+}
---
+++ src/Frontend/Menue.java
@@ -0,0 +1,43 @@
+package Frontend;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Menue {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menue window = new Menue();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Menue() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
