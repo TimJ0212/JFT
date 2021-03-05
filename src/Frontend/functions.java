aa50842aeebc375a60045b00a98bc4a3426d8ceb
ZeTr0nIx <jan.kuckuck@outlook.de>
2021-03-05 18:13:53 +0100

    Update functions.java

--- src/Frontend/functions.java
+++ src/Frontend/functions.java
@@ -7,17 +7,15 @@ import java.io.IOException;
 import java.text.ParseException;
 import org.json.simple.*;
 
-
 public class functions {
+
     public static void registerUSer(User user) throws IOException, ParseException {
-        String filepath = "testjson.json"; 
+        String filepath = "testjson.json";
         JSONObject obj1 = new JSONObject();
         JSONObject obj2 = new JSONObject();
         JSONObject temp = (JSONObject) readJSON(filepath);
         Object temp2 = temp.get("user");
         JSONArray arr = (JSONArray) temp2;
-
-
 
         obj2.put("email", user.email);
         obj2.put("password", user.password);
@@ -28,36 +26,34 @@ public class functions {
 
         writeJSON(arr, filepath);
     }
-    }
 
     public static Boolean login(String username, String password) throws IOException, ParseException {
 
         JSONObject temp = (JSONObject) readJSON("testjson.json");
         Object temp2 = temp.get("user");
         JSONArray arr = (JSONArray) temp2;
-        for(int i = 0; i<arr.size(); i++) {
+        for (int i = 0; i < arr.size(); i++) {
             JSONObject obj = (JSONObject) arr.get(i);
-            
-            if(obj.containsKey(username)){
+
+            if (obj.containsKey(username)) {
                 obj = (JSONObject) obj.get(username);
-                if(obj.get("password").equals(password)){
+                if (obj.get("password").equals(password)) {
                     return true;
                 }
             }
-            
         }
         return false;
     }
 
     public static Object readJSON(String filepath) throws IOException, ParseException {
         File file = new File(filepath);
-        
+
         FileReader fr = new FileReader(file);
         JSONParser jp = new JSONParser();
-        
+
         Object obj = jp.parse(fr);
         fr.close();
-        return(obj);
+        return (obj);
     }
 
     public static void writeJSON(JSONArray arr, String filepath) throws IOException {
